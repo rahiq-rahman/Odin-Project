22e88e5c9283f412b0a929763d55851abc5c6907
Rahiq Rahman <rahiqrahman2001@gmail.com>
2024-02-02 23:46:55 +0100

    added css to buttons

--- rock-paper-scissors/rock-paper-scissors.js
+++ rock-paper-scissors/rock-paper-scissors.js
@@ -1,32 +1,4 @@
 
-const play = playGame();
-
-function playGame(){
-    let countWins = 0;
-    let draw = 0;
-
-    alert("Hey!Let's play Rock Paper Scissor!!");
-
-    // for(i = 0; i < 5; i++){
-    //     let playerChoice = prompt("Choose:");
-    //     playerChoice = playerChoice.toLowerCase();
-
-    //     const computerChoice = getComputerChoice();
-    //     console.log("The computer choose "+ computerChoice);
-
-    //     const result = playRound(playerChoice,computerChoice);
-
-    //     if(result == "Won"){
-    //         countWins++;
-    //     }
-    //     else if(result == "Draw"){
-    //         draw++;
-    //     }
-    //     console.log("You " + result + "!");
-    // }
-    // console.log("The final score is: " + countWins + "-" + (5 - countWins - draw) + " with " + draw + " draws");
-}
-
 function getComputerChoice(){
     let randomNum = Math.ceil(Math.random() * 3);
     let choice;
@@ -38,7 +10,7 @@ function getComputerChoice(){
         choice = "paper";
     }
     else 
-        choice = "scissor";
+        choice = "scissors";
 
     return choice;
 }
@@ -46,13 +18,41 @@ function getComputerChoice(){
 function playRound(playerSelection, computerSelection){
     const player = playerSelection;
     const computer = computerSelection;
-
+    
     if (player == computer){
-        console.log( "Draw");
+        console.log("Draw");
     }
-    else if (player == "rock" && computer == "scissor" || player == "paper" && computer == "rock" || player == "scissor" && computer == "paper"){
-        console.log( "Won");
+    else if (player == "rock" && computer == "scissors" || player == "paper" && computer == "rock" || player == "scissors" && computer == "paper"){
+        console.log("Won");
     }
     else
-        console.log( "Lost");
+    console.log("Lost");
 }
+    
+    // const play = playGame();
+    
+    // function playGame(){
+    //     let countWins = 0;
+    //     let draw = 0;
+    
+    //     alert("Hey!Let's play Rock Paper Scissor!!");
+    
+    //     // for(i = 0; i < 5; i++){
+    //     //     let playerChoice = prompt("Choose:");
+    //     //     playerChoice = playerChoice.toLowerCase();
+    
+    //     //     const computerChoice = getComputerChoice();
+    //     //     console.log("The computer choose "+ computerChoice);
+    
+    //     //     const result = playRound(playerChoice,computerChoice);
+    
+    //     //     if(result == "Won"){
+    //     //         countWins++;
+    //     //     }
+    //     //     else if(result == "Draw"){
+    //     //         draw++;
+    //     //     }
+    //     //     console.log("You " + result + "!");
+    //     // }
+    //     // console.log("The final score is: " + countWins + "-" + (5 - countWins - draw) + " with " + draw + " draws");
+    // }
