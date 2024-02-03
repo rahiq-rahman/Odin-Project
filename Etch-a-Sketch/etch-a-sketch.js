01f6c110d60c70429ebba6165d9ab52dc4cdbc31
Rahiq Rahman <rahiqrahman2001@gmail.com>
2024-02-03 19:05:09 +0100

    added hover effect

--- Etch-a-Sketch/etch-a-sketch.js
+++ Etch-a-Sketch/etch-a-sketch.js
@@ -1,13 +1,19 @@
 let container = document.getElementById("container");
 
-
 function createGrid(){
     let size = prompt("Enter grid size: ");
-    while(size > 101){
-        size = prompt("Grid Size should be less then 100.Enter grid size: ");
-    }
 
-    for(let i = 0; i < size*size ; i++){
-        container.innerHTML += '<div class="grid"></div>';
+    while(size > 50){
+        size = prompt("Grid Size should be less then 50.Enter grid size: ");
     }
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+    }
+    for(let i = 0; i < size*size ; i++){
+        container.innerHTML += '<div class="grid" onmouseover="changeBackground(this)"></div>';
+    }
+}
+
+function changeBackground(x){
+    x.style.backgroundColor = 'red';
 }
