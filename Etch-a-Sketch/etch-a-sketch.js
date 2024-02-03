9b7763f4e87c78b1009d05c1d6caef33a2dfe14d
Rahiq Rahman <rahiqrahman2001@gmail.com>
2024-02-03 17:26:09 +0100

    made user choose grid size with prompt

--- Etch-a-Sketch/etch-a-sketch.js
+++ Etch-a-Sketch/etch-a-sketch.js
@@ -2,13 +2,12 @@ let container = document.getElementById("container");
 
 
 function createGrid(){
-    const size = prompt("Enter grid size: ");
-    if(size < 101){
-
-        for(let i = 0; i < size*size ; i++){
-            container.innerHTML += '<div class="grid"></div>';
-        }
-    }
-    else 
+    let size = prompt("Enter grid size: ");
+    while(size > 101){
         size = prompt("Grid Size should be less then 100.Enter grid size: ");
+    }
+
+    for(let i = 0; i < size*size ; i++){
+        container.innerHTML += '<div class="grid"></div>';
+    }
 }
